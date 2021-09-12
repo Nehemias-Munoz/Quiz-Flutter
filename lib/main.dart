3c925ea6da1fe97fbf04f5d9a0e29b437b4c3bfa
Nehemias-Munoz <nehemiasmarvic.dev@gmail.com>
2021-09-12 20:47:18 -0300

    final appearance

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Material App Bar'),
-          ),
-          body: QuizApp()),
+        backgroundColor: Colors.grey[600],
+        body: SafeArea(child: QuizApp()),
+      ),
     );
   }
 }
@@ -28,7 +27,57 @@ class _QuizAppState extends State<QuizApp> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: null,
+      padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 5,
+            child: Center(
+              child: Text(
+                'Questions',
+                style: TextStyle(fontSize: 23.0, color: Colors.white),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.green)),
+              child: Text(
+                'True',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              onPressed: () {},
+            ),
+          ),
+          SizedBox(height: 15.0),
+          Expanded(
+            flex: 1,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red[400])),
+              child: Text(
+                'False',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              onPressed: () {},
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Row(
+            children: [
+              Icon(
+                Icons.check,
+                color: Colors.green,
+              ),
+              Icon(Icons.dangerous_outlined, color: Colors.red),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
